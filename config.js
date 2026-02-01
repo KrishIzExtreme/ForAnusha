3f061a335651838aab607c8cd91196fb3e2af025
KrishIzExtreme <krish993000@gmail.com>
2026-02-01 23:04:52 +0530

    Update config.js

--- config.js
+++ config.js
@@ -61,7 +61,7 @@ const CONFIG = {
         backgroundEnd: "#ffc3a0",        // Gradient end (should complement backgroundStart)
         buttonBackground: "#ff6b6b",     // Button color (should stand out against the background)
         buttonHover: "#ff8787",          // Button hover color (slightly lighter than buttonBackground)
-        textColor: "#ff4757"             // Text color (make sure it's readable!)
+        textColor: "#C0C999"             // Text color (make sure it's readable!)
     },
 
     // Animation settings
